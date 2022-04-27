987ad7b33aee8787705f423df1929c0ef57ba56f
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2022-04-27 17:39:27 +0700

    up

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './Component/MenuComponent.js';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+      <Navbar dark color="primary">
+        <div className='container'>
+            <NavbarBrand href="/"> Ristorante Con Fusion
+            </NavbarBrand>
+        </div>
+
+      </Navbar>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
